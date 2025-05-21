451949a56072230210b0d2f44dafbd02ad6d5bd5
Joseph <parsnip1966@gmail.com>
2025-05-21 12:47:51 +0100

    Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    def last_block(self):
+        pass
