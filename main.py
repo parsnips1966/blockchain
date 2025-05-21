05ac377339145c4fa9364da81a9a575da803f092
Joseph <parsnip1966@gmail.com>
2025-05-21 13:35:27 +0100

    Commit

--- main.py
+++ main.py
@@ -1,17 +1,65 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        pass
+        self.new_block(100, 1)
 
-    def new_transaction(self):
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": time(),
+            "transactions": self.current_transaction,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.chain[-1])
+        }
+
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+        self.current_transactions.append({
+            "sender": sender,
+            "recipient": recipient,
+            "amount": amount
+        })
+        return self.last_block["index"] + 1
 
     @staticmethod
     def hash(block):
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+        print(block)
+        print(json.dumps(block, sort_keys=True))
+        block_string = json.dumps(block, sort_keys=True).encode()
+        print(block_string)
+        return hashlib.sha256(block_string).hex_digest()
 
+    @property
     def last_block(self):
-        pass
+        return self.chain[-1]
+
+if __name__ == "__main__":
+    blockchain = Blockchain()
